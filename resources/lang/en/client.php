d772edb862c9f6b0adfb23a5f78216c59f1efaf0
jeromedh <jdieuhou@yahoo.fr>
2020-10-23 12:34:43 +0100

    Languages files

---
+++ resources/lang/en/client.php
@@ -0,0 +1,98 @@
+<?php
+
+return [
+
+    /**
+     * The messages for client
+     */
+
+    // About
+    'welcome_portfolio' => 'Welcome to my Portfolio',
+
+    // Experiencies
+    'experiencies' => 'Experiences',
+    'experiencyTitle' => 'My experiences and related works',
+    'experiencyDesc' => 'There are my experiences over the seven years old and many related works',
+    'experiencyAuthor' => "Jerome Dh",
+    'experiency_motivation' => 'My long experiences apply to web technologies and mobile works',
+
+    // Skills and works
+    'skills' => 'Skills',
+    'skillTitle' => 'My skills',
+    'skillDesc' => 'There are my skills',
+    'skillAuthor' => "Jerome Dh",
+    'skills_motivation' => 'Tools, Technologies and programming languages are my motivations for long time ago',
+
+    // Works
+    'works' => 'Works',
+    'related' => 'My related & personals works',
+    'works_motivation' => 'The works conspired to personals projects as also as skills complement',
+
+    // Others
+    'others' => 'Others',
+    'othersTitle' => 'Documentations and works books',
+    'othersDesc' => 'There are my documentations and useful links',
+    'othersAuthor' => 'Jerome Dh',
+    'othersRelated' => 'Useful links',
+    'books_read' => 'Documentations and books',
+    'others_related' => 'More information & Useful links',
+
+    // Blog
+    'blog' => 'Blog',
+    'blogDesc' => 'Following astuces and skills technologies',
+    'blogAuthor' => 'Jerome Dh',
+    'blogH1' => 'Blog',
+
+    'usefull_link' => 'Useful links',
+    'language' => 'Language',
+    'navigation' => 'Navigation',
+    'socials' => 'Socials links',
+    'up' => 'Go to top',
+    'english' => 'English',
+    'francais' => 'Francais',
+    'change_language' => 'Choose language',
+    'change_lang_text' => 'Select another language to navigate',
+    'about' => 'About',
+    'ok' => 'ok',
+    'view' => 'View',
+    'description' => 'Description',
+    'techno_used' => 'Technologies used',
+    'medias' => 'Medias',
+    'source' => 'Source',
+    'today' => 'Today',
+    'title' => 'Title',
+    'more' => 'More',
+    'see_more' => 'See more',
+
+    'about_p1' => "<strong>Hi, my name is Jerome Dh and this is my portfolio.</strong><br>
+            I give some <a href=':skills_url'>skills</a> and <a href=':works_url'>related works</a>  in this portfolio as
+            also as my many <a href=':experiencies_url'>experiences</a>.",
+    'about_p6' => "Otherwise others information such as my scholarship or some evaluations and certifications can be found on
+            my <a href='".env('MY_LINKEDIN')."' target='_blank' class='uk-link'>Linkedin</a> profile
+            or others related works on <a href='".env('MY_GITHUB')."' target='_blank' class='uk-link'>Github</a>",
+
+    'about_p2' => "I typically work with <strong>web and mobiles technologies</strong> for services given to enterprises,
+            this services are oriented on management, finances, invoicing, etc..
+            but during my own researches and hobbies I often make applications for desktop such my
+            <a href='".env('MY_GITHUB')."/web-server' target='_blank' class='uk-link'>web server</a>.",
+
+    'about_p3' => "So, I hardly work on large application such web Server-client, mobile application, Database and API.
+            I don't very have preferred technology or language but I more time worked with <strong>PHP, Java, JavaScript and Android</strong>",
+    'about_p4' => "Here, I put together some detailed information that can see in others pages:",
+    'about_p5' => "<span uk-icon='info'></span> Not forget to see many others information on <a href='".env('MY_GITHUB')."' target='_blank' class='uk-link'>Github</a> or
+        <a href='".env('MY_LINKEDIN')."' target='_blank' class='uk-link'>Linkedin</a>",
+    'about_em1' => "<em>Thank's visiting my portfolio</em>",
+    'about_h3_1' => "About me",
+    'about_h3_2' => "Motivations",
+    'about_h3_3' => "What found in this portfolio",
+    'about_ul_1' => "<li><a href=':experiencies_url'>My experiences</a>, which I've obtained over the seven years old</li>
+            <li><a href=':works_url'>My related works</a> such as my works doing on my scholarship aim or in my favorite pass time.</li>
+            <li><a href=':skills_url'>My skills</a> obtained more time on my experiences, my own research or to schools</li>
+            <li>Can also see <a href=':books_url'>some books</a> I learn many time to works with such technologies</li>
+            <li>By lake of time, I can't maintain a <a href=':blog_url'>blog</a> now, but it's coming soon !</li>",
+
+    'og_description' => 'This is the Jerome Dh Portfolio. Developer PHP, Java, JavaScript and associated frameworks',
+    'head_title' => "PHP, Java and JavaScript Developer",
+    'average' => 'average',
+
+];
---
+++ resources/lang/fr/client.php
@@ -0,0 +1,103 @@
+<?php
+
+return [
+
+    /**
+     * The messages for client
+     */
+
+    // About
+    'welcome_portofio' => 'Bienvenue dans mon Portfolio',
+
+    // Experiencies
+    'experiencies' => 'Experiences',
+    'experiencyTitle' => 'Experiences et travaux relatifs',
+    'experiencyDesc' => "Six annees d'experiences dans divers travaux",
+    'experiencyAuthor' => "Jerome Dh",
+    'experiency_motivation' => "Des longues annees d'experiences obtenues dans les technologies web et mobile",
+
+    // Skills and works
+    'skills' => 'Competences',
+    'skillTitle' => 'Mes competences',
+    'skillDesc' => 'Mes competences',
+    'skillAuthor' => "Jerome Dh",
+    'skills_motivation' => 'Les technologies et outils associes sont mes motivations depuis six longues annees',
+
+    // Works
+    'works' => 'Travaux',
+    'related' => 'Travaux personnels & relatifs',
+    'works_motivation' => "Les travaux consacres a des recherches personnelles et scolaires",
+
+    // Others
+    'others' => 'Autres',
+    'othersTitle' => 'Documentations et livres',
+    'othersDesc' => 'Quelques documentations et livres exploitess',
+    'othersAuthor' => 'Jerome Dh',
+    'othersRelated' => 'Liens utiles',
+    'books_read' => 'Documentations et livres',
+    'others_related' => "Plus d'informations & liens utiles",
+
+    // Blog
+    'blog' => 'Blog',
+    'blogDesc' => 'Partages des astuces et connaissances autour des technologies',
+    'blogAuthor' => 'Jerome Dh',
+    'blogH1' => 'Blog',
+
+    'usefull_link' => 'Liens utiles',
+    'language' => 'Langue',
+    'navigation' => 'Navigation',
+    'socials' => 'Liens sociaux',
+    'up' => 'Vers le haut',
+    'english' => 'English',
+    'francais' => 'Francais',
+    'change_language' => 'Choisir la langue',
+    'change_lang_text' => 'Selectionner une langue de navigation',
+    'about' => 'A propos',
+    'ok' => 'ok',
+    'view' => 'Voir',
+    'description' => 'Description',
+    'techno_used' => 'Technologies utilisees',
+    'medias' => 'Medias',
+    'source' => 'Source',
+    'today' => "Aujourd'hui",
+    'title' => 'Titre',
+    'more' => 'Plus',
+    'see_more' => 'Voir plus',
+
+    'about_p1' => "<strong>Salut, je suis Jerome Dh et ce site presente mon Portfolio.</strong><br>
+            Celui-ci fait references a mes <a href=':skills_url'>competences</a>, des <a href=':works_url'>travaux relatifs</a> et aussi bien
+            a mes <a href=':experiencies_url'>experiences</a>.",
+
+    'about_p6' => "Autrement, certaines informations telle que mon parcours academique ou mes diplomes et certifications peuvent etre trouvees
+            sur mon profil <a href='".env('MY_LINKEDIN')."' target='_blank' class='uk-link'>Linkedin</a> et
+            des travaux relatifs sur <a href='".env('MY_GITHUB')."' target='_blank' class='uk-link'>Github</a>.",
+
+    'about_p2' => "Je travaille typiquement sur les <strong>technologies web et mobiles</strong> pour les offres de services aux entreprises.
+            Ces services sont axees sur la gestion, les finances, la facturation, etc..
+            mais durang mes propres recherches et mes passe-temps, je travaille sur des applications orientees Desktop tel que le projet
+            <a href='".env('MY_GITHUB')."/web-server' target='_blank' class='uk-link'>web server</a>.",
+
+    'about_p3' => "Donc, je travaille le plus souvent sur des applications client-serveur, les applications mobiles, les bases de donnees et les API.
+            Je n'ai veritablement pas de langage ou technologie preferee mais habituellement je fais des realisations avec les langages <strong>PHP, Java, JavaScript, Android</strong>.",
+
+    'about_p4' => "Ici, j'expose des liens vers les autres pages pour des informations detaillees sur mes experiences, travaux et competences:",
+    'about_p5' => "<span uk-icon='info'></span> Plus d'informations peuvent etre obtenues sur <a href='".env('MY_GITHUB')."' target='_blank' class='uk-link'>Github</a> et
+            <a href='".env('MY_LINKEDIN')."' target='_blank' class='uk-link'>Linkedin</a>.",
+
+    'about_em1' => "<em>Merci pour votre visite</em>",
+
+    'about_h3_1' => "A propos",
+    'about_h3_2' => "Motivations",
+    'about_h3_3' => "Quels contenus ?",
+
+    'about_ul_1' => "<li><a href=':experiencies_url'>Des experiences</a> obtenues au travers de plusieurs projets</li>
+            <li><a href=':works_url'>Des travaux relatifs</a>, tels que les projets academiques et open-sources</li>
+            <li><a href=':skills_url'>Des competences</a> acquises au long des experiences, des propres recherches ou durang mon parcours academique</li>
+            <li>Aussi, <a href=':books_url'>plusieurs documents et liens</a> que j'exploite regulierement durang mes travaux</li>
+            <li>Par manque de temps, il m'est actuellement difficile d'entretenir un <a href=':blog_url'>blog</a>, mais cela est pour bientot !</li>",
+
+    'og_description' => 'Porfolio Jerome Dh. Developpeur PHP, Java, JavaScript et frameworks associes',
+    'head_title' => "Developpeur PHP, Java, JavaScript",
+    'average' => 'Moyenne',
+
+];
